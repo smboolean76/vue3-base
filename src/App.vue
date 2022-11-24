33b3bdc30fcfbd8786990214a8ecdb13c3722ae5
smboolean76 <smboolean76@madrigali.dev>
2022-11-24 12:37:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <HelloWorld />
+  </div>
+  <div class="container-small">
+    <HelloWorld />
+  </div>
+</template>
+
+<style lang="scss">
+@import './style/global.scss';
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style>
+</style>
